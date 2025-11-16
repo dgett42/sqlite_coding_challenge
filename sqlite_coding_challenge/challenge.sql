38157fb58d7b283b724dd284abe0a1b87a15de96
Donovan <degeter1@usf.edu>
2025-11-16 18:11:59 -0500

    Comleted task 1

--- sqlite_coding_challenge/challenge.sql
+++ sqlite_coding_challenge/challenge.sql
@@ -1,2 +1,17 @@
 SELECT * FROM customers LIMIT 5;
 
+-- TASK 1 — Top 5 Customers by Total Spend
+
+SELECT
+    c.first_name || ' ' || c.last_name AS customer_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM customers c
+JOIN orders o
+    ON o.customer_id = c.id
+JOIN order_items oi
+    ON oi.order_id = o.id
+GROUP BY
+    c.id, c.first_name, c.last_name
+ORDER BY
+    total_spend DESC
+LIMIT 5;
